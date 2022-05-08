c7eff7756c39a7dfb663bf08ccdd1b098d91f8ea
AlEscobedo <al.escobedo@duocuc.cl>
2022-05-07 20:11:08 -0400

    a

--- JS/IniciarSesion.js
+++ JS/IniciarSesion.js
@@ -16,18 +16,7 @@ $(document).ready(function(){
         let entrarMensajeClave = false;
 
 
-        //VALIDACION INPUT USUARIO
-        if(usuario.trim().length < 4 || usuario.trim().length > 10){
-            mensajeUsuario += "El nombre de usuario debe tener entre 4 y 10 caracteres.";
-            entrarMensajeUsuario = true;
-        }
-
-        if(entrarMensajeUsuario){
-            $("#mensajeUsuario").html(mensajeUsuario);
-        }
-        else{
-            $("#mensajeUsuario").html(mensajeUsuario);
-        }
+       
         //FIN VALIDACION INPUT USUARIO
     })
 })
